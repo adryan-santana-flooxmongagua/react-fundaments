5e143568e67d5515de08538c8771754faac42470
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-13 10:34:51 -0300

    feat: implement React Fragment in components

--- app/src/index.js
+++ app/src/index.js
@@ -2,17 +2,19 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basic/Primeiro'
+import First from './components/basic/First'
 import ComParametro from './components/basic/ComParametro'
+import Fragment from './components/basic/Fragmento'
 
 const tag = <strong>Olá react!</strong> 
-
 ReactDom.render(
     <div id="app">
-        <Primeiro></Primeiro>
+        <First></First>
         <ComParametro 
             titulo="Situação do Aluno" 
-            subtitulo="Lopez" nota={9.3} />
+            subtitulo="Lopez" 
+            nota={9.3} />
+            <Fragment/>
     </div>,
     document.getElementById('root')
 )
