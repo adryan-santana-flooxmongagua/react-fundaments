63136aa8def7cadd9837b3aac6b094fb365fdabe
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-13 10:10:59 -0300

    feat: add props support to component

--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,16 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basic/Primeiro'
+import ComParametro from './components/basic/ComParametro'
 
 const tag = <strong>Olá react!</strong> 
 
 ReactDom.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do Aluno" 
+            subtitulo="Lopez" nota={9.3} />
     </div>,
     document.getElementById('root')
 )
