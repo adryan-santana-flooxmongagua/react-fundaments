9684e76adab2832aaca14b30415fc2acfcaacb4b
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-14 10:30:28 -0300

    feat: create card component and pass content dynamically

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,15 +4,34 @@ import First from "./components/basic/First";
 import ComParametro from "./components/basic/ComParametro";
 import Fragment from "./components/basic/Fragmento";
 import Aleatory from "./components/basic/Aleatory";
+import Card from "./components/layout/Card";
 
 export default () => {
   return (
     <div id="app">
       <h1>Fundamentos do React 2</h1>
-      <Aleatory min={10} max={60}/>
-      <Fragment />
-      <ComParametro titulo="Situação do Aluno" subtitulo="Lopez" nota={9.3} />
-      <First />
+      <Card titulo="#04 - Desafio Aleatorio">
+      <Aleatory min={1} max={60}/>
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragment />
+      </Card>
+
+      <Card titulo="#02 - Com Parâmetro">
+        <ComParametro 
+        titulo="Situação do Aluno" 
+        subtitulo="Lopez" 
+        nota={9.3} />
+      </Card>
+
+      <Card titulo="#01 - Primeiro">
+       <First />
+      </Card>
+      
+     
+      
+   
     </div>
   );
 };
