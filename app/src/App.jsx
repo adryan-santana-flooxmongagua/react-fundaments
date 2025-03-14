c6246e8de38284ccaf4e5286aeeacd8573e330e1
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-14 10:41:24 -0300

    style: improve responsiveness using Flexbox

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 import ReactDom from "react-dom";
 import First from "./components/basic/First";
@@ -8,30 +9,30 @@ import Card from "./components/layout/Card";
 
 export default () => {
   return (
-    <div id="app">
+    <div className="App">
       <h1>Fundamentos do React 2</h1>
-      <Card titulo="#04 - Desafio Aleatorio">
-      <Aleatory min={1} max={60}/>
-      </Card>
 
-      <Card titulo="#03 - Fragmento">
-        <Fragment />
-      </Card>
+      <div className="Cards">
+        <Card titulo="#04 - Desafio Aleatorio">
+          <Aleatory min={1} max={60} />
+        </Card>
 
-      <Card titulo="#02 - Com Parâmetro">
-        <ComParametro 
-        titulo="Situação do Aluno" 
-        subtitulo="Lopez" 
-        nota={9.3} />
-      </Card>
+        <Card titulo="#03 - Fragmento">
+          <Fragment />
+        </Card>
 
-      <Card titulo="#01 - Primeiro">
-       <First />
-      </Card>
-      
-     
-      
-   
+        <Card titulo="#02 - Com Parâmetro">
+          <ComParametro
+            titulo="Situação do Aluno"
+            subtitulo="Lopez"
+            nota={9.3}
+          />
+        </Card>
+
+        <Card titulo="#01 - Primeiro">
+          <First />
+        </Card>
+      </div>
     </div>
   );
 };
