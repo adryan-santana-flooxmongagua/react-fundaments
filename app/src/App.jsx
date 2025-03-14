822ededd9a04c0832dea05dbfd6674f20d46d7e3
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-03-14 09:25:51 -0300

    feat: add random number generator

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,11 +3,13 @@ import ReactDom from "react-dom";
 import First from "./components/basic/First";
 import ComParametro from "./components/basic/ComParametro";
 import Fragment from "./components/basic/Fragmento";
+import Aleatory from "./components/basic/Aleatory";
 
 export default () => {
   return (
     <div id="app">
       <h1>Fundamentos do React 2</h1>
+      <Aleatory min={10} max={60}/>
       <Fragment />
       <ComParametro titulo="Situação do Aluno" subtitulo="Lopez" nota={9.3} />
       <First />
